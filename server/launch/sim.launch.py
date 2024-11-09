ca556b6f8759866c1b5ac9c9c98b3cfda37d555d
S1ink <samrichter888@gmail.com>
2024-11-09 02:37:20 -0600

    debug tools

--- server/launch/sim.launch.py
+++ server/launch/sim.launch.py
@@ -10,6 +10,61 @@ from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 
+def make_scan_transformer(output_frame : str, override_frame : str, sub_topic, pub_topic):
+    return Node(
+        package = 'debug_tools',
+        executable = 'scan_transformer',
+        output = 'screen',
+        parameters = [{
+            'target_frame': output_frame,
+            'override_frame': override_frame
+        }],
+        remappings = [
+            ('input_scan', sub_topic),
+            ('transformed_scan', pub_topic)
+        ]
+    )
+
+def make_imu_transformer(output_frame : str, override_frame : str, sub_topic, pub_topic):
+    return Node(
+        package = 'debug_tools',
+        executable = 'imu_transformer',
+        output = 'screen',
+        parameters = [{
+            'target_frame': output_frame,
+            'override_frame': override_frame
+        }],
+        remappings = [
+            ('input_imu', sub_topic),
+            ('transformed_imu', pub_topic)
+        ]
+    )
+
+def make_imu_visualizer(topic):
+    return Node(
+        package = 'debug_tools',
+        executable = 'imu_visualizer',
+        output = 'screen',
+        parameters = [{ 'imu_topic': topic }]
+    )
+
+def make_accuracy_analyzer(active_frame : str = 'base_link', origin_frame : str = 'map', validation_frame : str = 'gz_base_link', sample_window : float = 0.25, is_sim : bool = True, remap = '/accuracy_analysis'):
+    return Node(
+        package = 'debug_tools',
+        executable = 'accuracy_analyzer',
+        output = 'screen',
+        parameters = [{
+            'origin_frame_id': origin_frame,
+            'active_frame_id': active_frame,
+            'validation_frame_id': validation_frame,
+            'std_sample_window_s': sample_window,
+            'use_sim_time': is_sim
+        }],
+        remappings = [
+            ('accuracy_analysis', remap)
+        ]
+    )
+
 def generate_launch_description():
 
     sim_pkg_path = get_package_share_directory('csm_gz_sim')
@@ -58,5 +113,7 @@ def generate_launch_description():
         launch_gazebo,
         launch_xbox_ctrl,
         tag_detector,
-        foxglove_node
+        foxglove_node,
+        make_accuracy_analyzer('base_link', 'map', 'gz_base_link', 0.25, True, 'localization_acc_analysis'),
+        make_accuracy_analyzer('base_link_e0', 'map', 'gz_base_link', 0.25, True, 'tags_detection_acc_analysis')
     ])
