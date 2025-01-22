b072259d00ff2f4c4a2d16b4fc96cdb9fd6c0584
Sam Richter <samrichter888@gmail.com>
2025-01-22 01:00:12 -0600

    work

--- lance/launch/sim.launch.py
+++ lance/launch/sim.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, AndSubstitution, NotSubstitution
 from launch.conditions import IfCondition
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -13,10 +13,13 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    pkg_path = get_package_share_directory('lance')
+    sim_pkg_path = get_package_share_directory('csm_sim')
+
     # launch robot_state_publisher using sim description
     robot_state_publisher = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('csm_sim'), 'launch', 'robot_state_publisher.launch.py')
+            os.path.join(sim_pkg_path, 'launch', 'robot_state_publisher.launch.py')
         ),
         launch_arguments = {'use_sim_time': 'true'}.items(),
         condition = IfCondition( LaunchConfiguration('use_state_pub', default='true') )
@@ -28,7 +31,7 @@ def generate_launch_description():
         executable = 'perception_node',
         output = 'screen',
         parameters = [
-            os.path.join(get_package_share_directory('lance'), 'config', 'perception_live.yaml'),
+            os.path.join(pkg_path, 'config', 'perception_live.yaml'),
             {
                 'use_sim_time': True,
                 'scan_topic': '/lance/lidar_scan',
@@ -44,17 +47,56 @@ def generate_launch_description():
     # foxglove server if enabled
     foxglove_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('lance'), 'launch', 'foxglove.launch.py')
+            os.path.join(pkg_path, 'launch', 'foxglove.launch.py')
         ),
         launch_arguments = {'use_sim_time': 'true'}.items(),
         condition = IfCondition(LaunchConfiguration('foxglove', default='true'))
+    )
+
+    isaac_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sim_pkg_path, 'launch', 'isaac_sim.launch.py')
+        ),
+        launch_arguments = {
+            'state_pub' : 'false',
+            'foxglove' : 'false'
+        }.items(),
+        condition = IfCondition(
+            AndSubstitution(
+                LaunchConfiguration('isaac_sim'),
+                NotSubstitution(
+                    LaunchConfiguration('gz_sim')
+                )
+            )
+        )
+    )
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sim_pkg_path, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments = {
+            'state_pub' : 'false',
+            'foxglove' : 'false'
+        }.items(),
+        condition = IfCondition(
+            AndSubstitution(
+                LaunchConfiguration('gz_sim'),
+                NotSubstitution(
+                    LaunchConfiguration('isaac_sim')
+                )
+            )
+        )
     )
 
 
     return LaunchDescription([
         DeclareLaunchArgument('use_state_pub', default_value='true'),
         DeclareLaunchArgument('foxglove', default_value='true'),
+        DeclareLaunchArgument('isaac_sim', default_value='false'),
+        DeclareLaunchArgument('gz_sim', default_value='false'),
         robot_state_publisher,
         launch_localization,
-        foxglove_node
+        foxglove_node,
+        isaac_sim,
+        gz_sim
     ])
