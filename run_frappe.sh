09629f0be167dad54c9f5e45c7b2577c692c1123
Sam Richter <samrichter888@gmail.com>
2025-05-17 09:25:19 -0500

    fix relative script paths

--- run_frappe.sh
+++ run_frappe.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-$cwd/motor-control/scripts/can_bringup.sh
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+$SCRIPTPATH/motor-control/scripts/can_bringup.sh
+
 source install/setup.bash
 ros2 launch lance robot.launch.py foxglove:=false foxglove_mode:=live perception:=false record:=false phoenix_driver:=5 controller:=false
