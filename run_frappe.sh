9e43016c93d8d9caf17f05d5c128b56fcd8cbe39
Sam Richter <samrichter888@gmail.com>
2025-05-17 10:37:15 -0500

    submodules are good

--- run_frappe.sh
+++ run_frappe.sh
@@ -4,5 +4,5 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 $SCRIPTPATH/motor-control/scripts/can_bringup.sh
 
-source install/setup.bash
+# source install/setup.bash
 ros2 launch lance robot.launch.py foxglove:=false foxglove_mode:=live perception:=false record:=false phoenix_driver:=5 controller:=false
--- run_mocha.sh
+++ run_mocha.sh
@@ -4,5 +4,5 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 $SCRIPTPATH/motor-control/scripts/can_bringup.sh
 
-source install/setup.bash
+# source install/setup.bash
 ros2 launch lance robot.launch.py foxglove:=false foxglove_mode:=live perception:=true record:=false phoenix_driver:=6 controller:=true
