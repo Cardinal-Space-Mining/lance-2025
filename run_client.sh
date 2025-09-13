2c27880278d0b274740a64d364c71b0e060ad335
Sam Richter <samrichter888@gmail.com>
2025-09-13 20:20:25 +0000

    add presets and update scripts

--- run_client.sh
+++ run_client.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-FG_BRIDGE_MODE=live
-MOTOR_LOGGING=true
-LIDAR_LOGGING=false
-
-# local bridge mode starts the robot state publisher and foxglove bridge nodes on the client rather than robot
-LOCAL_BRIDGE=true
-if [[ "$1" == "--remote-bridge" ]]; then
-    echo "Remote bridge mode set. NOT running state publisher and foxglove bridge locally."
-    LOCAL_BRIDGE=false
-fi
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 source $SCRIPTPATH/../install/setup.bash
 
-ros2 launch lance client.launch.py \
-    foxglove_gui:=true \
-    foxglove_bridge:=$LOCAL_BRIDGE \
-    foxglove_bridge_mode:=$FG_BRIDGE_MODE \
-    enable_state_pub:=$LOCAL_BRIDGE \
-    record_motor:=$MOTOR_LOGGING \
-    record_lidar:=$LIDAR_LOGGING
+BASE_CMD=(
+    ros2
+    launch
+    lance
+    lance.launch.py
+    preset:=client
+)
+"${BASE_CMD[@]}" "$@"
--- run_mocha.sh
+++ run_mocha.sh
@@ -34,62 +34,20 @@ fi
 
 #--------------------------------------------------
 
-# Default values
-RUN_PERCEPTION=false
-FG_ENABLED=true
-FG_BRIDGE_MODE=test
-LIDAR_LOGGING=false
-MOTOR_LOGGING=true
-
-RUN_FG=$FG_ENABLED
-DISABLE_STATE_PUB=false
-
-# Parse named arguments
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    perception=*)
-      RUN_PERCEPTION="${1#*=}"
-      shift
-      ;;
-    lidar-logging=*)
-      LIDAR_LOGGING="${1#*=}"
-      shift
-      ;;
-    --client-bridge)
-      RUN_FG=false
-      DISABLE_STATE_PUB=true
-      shift
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
-done
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-$SCRIPTPATH/motor-control/scripts/can_bringup.sh
-
-# echo "RUN_FG: $RUN_FG"
-# echo "FG_BRIDGE_MODE: $FG_BRIDGE_MODE"
-# echo "RUN_PERCEPTION: $RUN_PERCEPTION"
-# echo "DISABLE_STATE_PUB: $DISABLE_STATE_PUB"
+$SCRIPTPATH/phoenix-driver/scripts/can_bringup.sh
 
 source $SCRIPTPATH/../install/setup.bash
 
-# source install/setup.bash
-ros2 launch lance robot.launch.py \
-    foxglove:=$RUN_FG \
-    foxglove_mode:=$FG_BRIDGE_MODE \
-    perception:=$RUN_PERCEPTION \
-    record_lidar:=$LIDAR_LOGGING \
-    record_motor:=$MOTOR_LOGGING \
-    disable_state_pub:=$DISABLE_STATE_PUB \
-    phoenix_driver:=6 \
-    controller:=true \
-    arduino_device:=$ARDUINO_DEVICE \
-    force_lidar_driver:=$LIDAR_LOGGING
+BASE_CMD=(
+    ros2
+    launch
+    lance
+    lance.launch.py
+    preset:=mocha
+)
+"${BASE_CMD[@]}" "$@"
 
 $SCRIPTPATH/motor-control/scripts/can_shutdown.sh
